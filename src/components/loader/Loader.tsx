48bea1bbf1ff8805f8471543a2a7ee9b9df93017
gasparovicsniki <gasparovics.niki@icloud.com>
2024-10-28 23:39:36 +0100

    Add Loader component and integrate with Page component for API loading state

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loader.scss"; // Include this file to style the loader
+
+const Loader = () => (
+  <div className="loader">
+    <div className="spinner"></div>
+    <p>Loading...</p>
+  </div>
+);
+
+export default Loader;
